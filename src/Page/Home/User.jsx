a5e8004e72fa64bf49be722fca639f21518d4f7f
NieamulKabir <kabir000079@gmail.com>
2024-02-10 20:05:59 +0600

    add search input

--- src/Page/Home/User.jsx
+++ src/Page/Home/User.jsx
@@ -1,19 +1,28 @@
-
-import useUsers from '../../hooks/useUsers';
-import UserListCard from '../../components/UserListCard';
+import useUsers from "../../hooks/useUsers";
+import UserListCard from "../../components/UserListCard";
+import SearchUser from "./SearchUser";
+import { useState } from "react";
 
 const User = () => {
   const [usersData] = useUsers();
+  const [filteredUsers, setFilteredUsers] = useState([]);
   const users = usersData?.users?.slice(0, 6);
-  // console.log(users);
-  // console.log(users.users);
+
+  function handleSearch(searchTerm) {
+    const filtered = users.filter((user) =>
+      user.firstName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredUsers(filtered);
+  }
+  console.log(users);
 
   return (
     <div className="min-h-screen max-w-7xl mx-auto">
       <h1 className="text-center text-xl md:text-4xl  font-bold mt-10 mb-4">
         User Lists
       </h1>
-      <UserListCard users={users} />
+      <SearchUser  onSearch={handleSearch}/>
+      <UserListCard users={filteredUsers.length > 0 ? filteredUsers : users} />
     </div>
   );
 };
